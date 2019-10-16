db2248acdb44a0064bba801c3b6eb91ee812cc61
Filipe Guerra <filipehmguerra@yahoo.com.br>
2019-10-16 01:20:21 -0300

    decision tree regression

--- machineLearning.py
+++ machineLearning.py
@@ -29,3 +29,30 @@ melbourne_model.fit(X, y)
 # Predictions
 print("The predictions are")
 print(melbourne_model.predict(X.head()))
+
+
+# MEAN ABSOLUTE ERROR
+from sklearn.metrics import mean_absolute_error
+
+predicted_home_prices = melbourne_model.predict(X)
+mean_absolute_error(y, predicted_home_prices)
+
+
+# SPLITING DATA TO TRAINING
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_X, train_y)
+
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
+
+# UNDERFITTING AND OVERFITTING
